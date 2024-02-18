d95e7dcbd002625743b2c1ccd1870a755b8da2fe
Kirill <4cd87a@gmail.com>
2024-02-18 20:33:21 +0100

    update setup

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="driverlib",
-    version="0.0.1",
+    version="0.0.2",
     author="LKB",
     author_email="cryo.paris.su@gmail.com",
     description="Control your instruments via USB or LAN.",
