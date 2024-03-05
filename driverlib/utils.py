2f1e0187c6ea52318cf070ebd7e61c6470287089
Remi Rousseau <cryo.paris.su@gmail.com>
2024-03-05 12:54:31 +0100

    add any annotation attr in allow keys

--- driverlib/utils.py
+++ driverlib/utils.py
@@ -3,8 +3,10 @@ from typing import Any
 
 class LimitedAttributeSetter:
     def __setattr__(self, __name: str, __value: Any) -> None:
-        if hasattr(self, __name) or (
-            hasattr(self, "_allow_attrs") and __name in self._allow_attrs
+        if (
+            hasattr(self, __name)
+            or __name in self.__annotations__.keys()
+            or (hasattr(self, "_allow_attrs") and __name in self._allow_attrs)
         ):
             super().__setattr__(__name, __value)
             return
