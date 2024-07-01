c43a730bf7c5719419ce20bac9f5163826c53ba7
Kirill <4cd87a@gmail.com>
2024-07-01 16:00:52 +0200

    update workflows

--- setup.py
+++ setup.py
@@ -2,19 +2,35 @@
 
 import setuptools
 
+NAME = "driverlib"
+AUTHOR = "kyrylo.gr | LKB-OMQ"
+AUTHOR_EMAIL = "git@kyrylo.gr"
+DESCRIPTION = "Control your instruments via USB or LAN."
+
+
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
+
+def get_version() -> str:
+    """Get version from __config__.py."""
+    with open(f"{NAME}/__config__.py", "r", encoding="utf-8") as file:
+        for line in file.readlines():
+            if line.startswith("__version__"):
+                return line.split("=")[1].strip().strip('"').strip("'")
+    raise ValueError("Version not found")
+
+
 setuptools.setup(
-    name="driverlib",
-    version="0.0.2",
-    author="LKB",
-    author_email="cryo.paris.su@gmail.com",
-    description="Control your instruments via USB or LAN.",
+    name=NAME,
+    version=get_version(),
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    description=DESCRIPTION,
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/kyrylo-gr/driverlib",
-    packages=setuptools.find_packages(exclude=["tests", "tests.*"]),
+    url=f"https://github.com/kyrylo-gr/{NAME}",
+    packages=setuptools.find_packages(exclude=["tests", "tests.*", "docs", "docs.*"]),
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
